cf8dc5d1b74121a4550a4650689db5a83cecf475
wussh <andrianta.321@gmail.com>
2024-04-17 15:50:50 +0700

    new file:   .env 	modified:   controller.go 	modified:   go.mod 	modified:   go.sum 	new file:   googlechat.go 	modified:   main.go

--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module kubecollect
 go 1.16
 
 require (
-	github.com/slack-go/slack v0.10.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	cloud.google.com/go/compute v1.23.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/slack-go/slack v0.10.0
+	golang.org/x/oauth2 v0.18.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	k8s.io/api v0.23.0
 	k8s.io/apimachinery v0.23.0
 	k8s.io/client-go v0.23.0
 	k8s.io/klog/v2 v2.30.0
-	k8s.io/kubectl v0.23.0 // indirect
-	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b // indirect
+	k8s.io/kubectl v0.23.0
+	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b
 )
---
+++ googlechat.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"k8s.io/klog/v2"
+)
+
+type GoogleChat struct {
+	WebhookUrl  string
+	ClusterName string
+	MuteSeconds int
+	History     map[string]time.Time
+}
+
+type GoogleChatMessage struct {
+	Text string `json:"text"`
+}
+
+func NewGoogleChat() GoogleChat {
+	var webhookUrl, clusterName string
+
+	if webhookUrl = os.Getenv("GOOGLE_CHAT_WEBHOOK_URL"); webhookUrl == "" {
+		klog.Exit("Environment variable GOOGLE_CHAT_WEBHOOK_URL is not set")
+	}
+	if clusterName = os.Getenv("CLUSTER_NAME"); clusterName == "" {
+		clusterName = "cluster-name"
+		klog.Warningf("Environment variable CLUSTER_NAME is not set, default: %s\n", clusterName)
+	}
+	muteSeconds, err := strconv.Atoi(os.Getenv("MUTE_SECONDS"))
+	if err != nil {
+		muteSeconds = 600
+		klog.Warningf("Environment variable MUTE_SECONDS is not set, default: %d\n", muteSeconds)
+	}
+	klog.Infof("Clustername: %s, muteseconds: %d\n", clusterName, muteSeconds)
+
+	return GoogleChat{
+		WebhookUrl:  webhookUrl,
+		ClusterName: clusterName,
+		MuteSeconds: muteSeconds,
+		History:     make(map[string]time.Time),
+	}
+}
+
+func (g GoogleChat) sendToRoom(msg GoogleChatMessage) error {
+	// Marshal the message into JSON
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	// Send the HTTP POST request
+	resp, err := http.Post(g.WebhookUrl, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		klog.Infof("Error sending HTTP POST request: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		err := fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		klog.Infof("Unexpected status code: %d", resp.StatusCode)
+		return err
+	}
+
+	klog.Infof("Message sent successfully to Google Chat room")
+
+	return nil
+}
